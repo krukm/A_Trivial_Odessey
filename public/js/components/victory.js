71a243193c785500b4be5cb9aad83e386925cde6
Matthew Kruk <matthew.kruk@gmail.com>
2018-09-07 15:28:08 -0400

    redo empty commit

--- public/js/components/victory.js
+++ public/js/components/victory.js
@@ -3,12 +3,15 @@
 const victory = {
     template: `
     <h1>YOU WIN!!!</h1>
+    <button type="button" ng-click="resetGame();">Play again?</button>
     `,
-    controller: function () {
+    controller: ['$location', function ($location) {
         const vm = this;
 
-        
-    }
+        vm.resetGame = () => {
+            $location.path('/intro');
+        }
+    }]
 }
 
 angular.module('app').component('victory', victory);
