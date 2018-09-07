009149dbcbe05bc3f548dadf07e60653a524580f
Adrean1983 <adrean_@hotmail.com>
2018-09-07 15:42:10 -0400

    added radio buttons. Need to add dynamic classes

--- public/js/components/battle-ground.js
+++ public/js/components/battle-ground.js
@@ -5,32 +5,47 @@ const battleGround = {
         <button ng-click="$ctrl.getEasyTriviaQuestions()">Easy</button>
         <button ng-click="$ctrl.getMediumTriviaQuestions()">Medium</button>
         <button ng-click="$ctrl.getHardTriviaQuestions()">Hard</button>
-        <p> {{ $ctrl.easyQuestion.question }} </p>
-        <ul> 
-            <li ng-repeat="answer in $ctrl.easyQuestion">{{ answer }}</li>
-             
-        </ul>    
+        <p> {{ $ctrl.quizQuestion }} </p>
+        <form> 
+            <div ng-repeat="answer in $ctrl.answers">
+            <label ng-class="answer === $ctrl.correct_answer ? 'correct' : 'incorrect'">
+                <input ng-click="$ctrl.testRadio(answer)" type="radio" ng-model="response" ng-value="answer">
+                {{ answer }}
+            </label>
+            </div>
+        </from>    
     `,
+
     controller: ["TriviaService", function(TriviaService) {
         const vm = this;
 
         vm.getEasyTriviaQuestions = () => {
             TriviaService.getEasyQuestions().then((response) => {
-                console.log(response.results[0].question);
-                vm.easyQuestion = response.results[0].incorrect_answers;
-                vm.easyQuestion.push(response.results[0].correct_answer);
+                vm.correctAnswer = response.results[0].correct_answer;
+                vm.quizQuestion = response.results[0].question;
+                vm.answers = response.results[0].incorrect_answers;
+                vm.answers.push(response.results[0].correct_answer);
                 let array2 = [];
-                while (vm.easyQuestion.length !== 0) {
-              
-                  // Pick a remaining element...
-                  let randomIndex = Math.floor(Math.random() * vm.easyQuestion.length);
-                  array2.push(vm.easyQuestion[randomIndex]);
-                  vm.easyQuestion.splice(randomIndex, 1);
+                while (vm.answers.length !== 0) {
+                  let randomIndex = Math.floor(Math.random() * vm.answers.length);
+                  array2.push(vm.answers[randomIndex]);
+                  vm.answers.splice(randomIndex, 1);
                 }
-                vm.easyQuestion = array2;
-                console.log(vm.easyQuestion);
+                vm.answers = array2;
+                console.log(vm.answers);
             })
         }
+
+        vm.testRadio = (hit) => {
+            console.log(hit);
+            // console.log(response.results[0].correct_answer);
+            console.log(vm.correctAnswer);
+
+        }
+
+       
+
+
         vm.getMediumTriviaQuestions = () => {
             TriviaService.getMediumQuestions().then((response) => {
                 console.log(response);
