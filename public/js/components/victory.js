b19167ea80ca7d6dd2b3b2ccf89a8e12eb189970
Matthew Kruk <matthew.kruk@gmail.com>
2018-09-08 19:04:50 -0400

    add PlayerService and injected into victory.js

--- public/js/components/victory.js
+++ public/js/components/victory.js
@@ -5,10 +5,11 @@ const victory = {
     <h1>YOU WIN!!!</h1>
     <button type="button" ng-click="resetGame();">Play again?</button>
     `,
-    controller: ['$location', function ($location) {
+    controller: ['$location', 'PlayerService', function ($location, PlayerService) {
         const vm = this;
 
         vm.resetGame = () => {
+            PlayerService.resetPlayer();
             $location.path('/intro');
         }
     }]
