75a30a18600ae7bd3f94720213582b371d26ad3c
jalen <jalenparham97@gmail.com>
2018-09-09 15:08:02 -0400

    worked on styling the battle-ground component

--- public/js/components/battle-ground.js
+++ public/js/components/battle-ground.js
@@ -2,41 +2,45 @@
 
 const battleGround = {
     template: `
-        <button ng-click="$ctrl.getEasyTriviaQuestions()">Easy</button>
-        <button ng-click="$ctrl.getMediumTriviaQuestions()">Medium</button>
-        <button ng-click="$ctrl.getHardTriviaQuestions()">Hard</button>
-        <p> {{ $ctrl.quizQuestion }} </p>
-        <form> 
+    <section class="question__container">
+        <div><img class="zues" src="./img/zues.png"></div>
+        <p class="trivia__question"> {{ $ctrl.quizQuestion }} </p>
+        <form class="answers"> 
             <div ng-repeat="answer in $ctrl.answers" ng-class="{'answered': $ctrl.answered}" >
             <label ng-class="answer === $ctrl.correctAnswer ? 'correct' : 'incorrect'">
                 <input ng-click="$ctrl.userChooseAnswer(answer)" type="radio" ng-model="response" ng-value="answer">
                 {{ answer }}
             </label>
             </div>
-        </from>    
+        </from>
+    </section>
+    
+    <footer>
+        
+    </footer>
     `,
 
     controller: ["TriviaService", function(TriviaService) {
         const vm = this;
         vm.answered = false;
 
-        vm.getEasyTriviaQuestions = () => {
-            TriviaService.getEasyQuestions().then((response) => {
-                vm.correctAnswer = response.results[0].correct_answer;
-                vm.quizQuestion = response.results[0].question;
-                vm.answers = response.results[0].incorrect_answers;
-                vm.answers.push(response.results[0].correct_answer);
-                // let array2 = [];
-                // while (vm.answers.length !== 0) {
-                //   let randomIndex = Math.floor(Math.random() * vm.answers.length);
-                //   array2.push(vm.answers[randomIndex]);
-                //   vm.answers.splice(randomIndex, 1);
-                // }
-                // vm.answers = array2;
-                vm.answers.sort(function(a,b){return 0.5 - Math.random()});
-                console.log(vm.answers);
-            })
-        }
+        // vm.getEasyTriviaQuestions = () => {
+        TriviaService.getEasyQuestions().then((response) => {
+            vm.correctAnswer = response.results[0].correct_answer;
+            vm.quizQuestion = response.results[0].question;
+            vm.answers = response.results[0].incorrect_answers;
+            vm.answers.push(response.results[0].correct_answer);
+            // let array2 = [];
+            // while (vm.answers.length !== 0) {
+            //   let randomIndex = Math.floor(Math.random() * vm.answers.length);
+            //   array2.push(vm.answers[randomIndex]);
+            //   vm.answers.splice(randomIndex, 1);
+            // }
+            // vm.answers = array2;
+            vm.answers.sort(function(a,b){return 0.5 - Math.random()});
+            console.log(vm.answers);
+        })
+        // }
 
         vm.userChooseAnswer = (hit) => {
             console.log(hit);
