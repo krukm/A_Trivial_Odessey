45bef013fe166b9a3e76e2a82acdc6aec73a808d
chelsyehannum <chelsye.hannum@gmail.com>
2018-09-10 10:35:40 -0400

    fixed randomized question problem

--- public/js/components/battle-ground.js
+++ public/js/components/battle-ground.js
@@ -26,22 +26,23 @@ const battleGround = {
 
         // vm.getEasyTriviaQuestions = () => {
         TriviaService.getEasyQuestions().then((response) => {
-            vm.correctAnswer = response.results[0].correct_answer;
-            vm.quizQuestion = response.results[0].question;
-            vm.answers = response.results[0].incorrect_answers;
-            vm.answers.push(response.results[0].correct_answer);
-            // let array2 = [];
-            // while (vm.answers.length !== 0) {
-            //   let randomIndex = Math.floor(Math.random() * vm.answers.length);
-            //   array2.push(vm.answers[randomIndex]);
-            //   vm.answers.splice(randomIndex, 1);
-            // }
-            // vm.answers = array2;
-            vm.answers.sort(function(a,b){return 0.5 - Math.random()});
+                vm.questions = response.results;
+                vm.correctAnswer = response.results[0].correct_answer;
+                vm.quizQuestion = response.results[0].question;
+                vm.answers = response.results[0].incorrect_answers;
+                vm.answers.push(response.results[0].correct_answer);
+                // let array2 = [];
+                // while (vm.answers.length !== 0) {
+                //   let randomIndex = Math.floor(Math.random() * vm.answers.length);
+                //   array2.push(vm.answers[randomIndex]);
+                //   vm.answers.splice(randomIndex, 1);
+                // }
+                // vm.answers = array2;
+                vm.answers.sort(function(a, b) { return 0.5 - Math.random() });
 
-            console.log(vm.answers);
-        })
-        // }
+                console.log(vm.answers);
+            })
+            // }
 
         // vm.getAnotherQuestion = () => {
 
@@ -50,27 +51,28 @@ const battleGround = {
         vm.userChooseAnswer = (hit) => {
             console.log(hit);
             vm.answered = true;
-            if (hit === "Apollo") {
+            if (hit === vm.correctAnswer) {
                 vm.answered = false;
                 TriviaService.getEasyQuestions().then((response) => {
-                    vm.randomIndex = Math.floor(Math.random() * vm.answers.length)
+                    vm.questions = response.results;
+                    vm.randomIndex = Math.floor(Math.random() * vm.questions.length);
 
                     vm.correctAnswer = response.results[vm.randomIndex].correct_answer;
                     vm.quizQuestion = response.results[vm.randomIndex].question;
                     vm.answers = response.results[vm.randomIndex].incorrect_answers;
                     vm.answers.push(response.results[vm.randomIndex].correct_answer);
 
-                    vm.answers.sort(function(a,b){return 0.5 - Math.random()});
+                    vm.answers.sort(function(a, b) { return 0.5 - Math.random() });
 
-                    console.log(vm.answers);
+                    console.log(vm.questions.length);
                     console.log(`Random index is ${vm.randomIndex}`);
                 })
             }
-    
+
             console.log(vm.correctAnswer);
         }
 
-       
+
 
 
         vm.getMediumTriviaQuestions = () => {
@@ -82,9 +84,9 @@ const battleGround = {
             TriviaService.getHardQuestions().then((response) => {
                 console.log(response);
             });
-        }        
-    }]     
-};    
-    
+        }
+    }]
+};
+
 
 angular.module('app').component('battleGround', battleGround);
