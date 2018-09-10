ed74ade9575ace90d4ab5ffadc43a0bb7030ae8a
chelsyehannum <chelsye.hannum@gmail.com>
2018-09-10 09:49:44 -0400

    intro component

--- public/js/components/intro.js
+++ public/js/components/intro.js
@@ -2,12 +2,17 @@
 
 const intro = {
     template: `
+    <section> 
+    <p>Zeus has banished you to the Underworld and capriciously imprisoned your loved ones at Mt. Olympus. The gods it seems have made you their plaything! They will move heaven and earth so that you will never see your family again! Use your cunning, strength and bravery to defy the gods and foil their schemes.</p>
+    <button ng-click = "">START</button>
+    </section>
     
     `,
-    controller: function () {
+    controller: function() {
         const vm = this;
 
-        
+
+
     }
 }
 
