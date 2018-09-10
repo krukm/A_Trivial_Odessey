a4a184ea7eb073c44475445bfb17b2ee229b1998
Matthew Kruk <matthew.kruk@gmail.com>
2018-09-10 13:55:24 -0400

    add getters/ setters in PlayerService

--- public/js/PlayerService.js
+++ public/js/PlayerService.js
@@ -4,8 +4,23 @@ function PlayerService () {
     const vm = this;
     vm.playerHealth = 3;
 
+    vm.getPlayerHealth = () => {
+        return vm.playerHealth;
+    }
+
+    vm.setPlayerHealth = (value) => {
+        vm.playerHealth = value;
+    }
+
     vm.resetPlayer = () => {
         vm.playerHealth = 3;
+    }
+
+    vm.updateHealthDisplay = (id) => {
+        document.getElementById(id).innerHTML = "";
+        for (let i = 0; i < vm.playerHealth; i++) {
+            document.getElementById(id).insertAdjacentHTML('afterbegin', '<img class="img__heart" src="/public/img/heart.png"></img>');
+        }
     }
 }
 
