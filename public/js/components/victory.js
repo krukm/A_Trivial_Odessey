7db26c60a80895e8bb4317548e34f3c746401b0c
Matthew Kruk <matthew.kruk@gmail.com>
2018-09-11 16:27:56 -0400

    style victory page fix functionality

--- public/js/components/victory.js
+++ public/js/components/victory.js
@@ -3,17 +3,34 @@
 const victory = {
     template: `
     <section class="section__victory">
-    <h1 class="h1__winner">YOU WIN!!!</h1>
-    <button class="button__play-again" type="button" ng-click="resetGame();">Play again?</button>
+        <h1 class="h1__winner">YOU WIN!!!</h1>
+        <section class="section__wide-scroll">
+            <p class="p__credits" id="id__credits"></p>   
+        </section>
+        <button class="button__play-again" type="button" ng-click="$ctrl.resetGame();">Play again?</button>
     </section>
     `,
     controller: ['$location', 'PlayerService', function ($location, PlayerService) {
         const vm = this;
+        vm.i = 0;
+        vm.speed = 40;
 
         vm.resetGame = () => {
             PlayerService.resetPlayer();
             $location.path('/intro');
         }
+
+        vm.storyText = "You've completed your quest! The gods stood no chance before your awesome might. You delight in your family's reunion, and look to the horizon...";
+
+        vm.typeWriter = () => {
+            if (vm.i < vm.storyText.length) {
+                document.getElementById("id__credits").innerHTML += vm.storyText.charAt(vm.i);
+                vm.i++;
+                setTimeout(vm.typeWriter, vm.speed);
+            }
+        }
+
+        vm.typeWriter();
     }]
 }
 
