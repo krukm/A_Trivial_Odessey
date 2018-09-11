c8559d802430f3840366addf2ef011f8bfcc8529
Adrean1983 <adrean_@hotmail.com>
2018-09-10 23:14:26 -0400

    line added to map image and sized for desktop

--- public/js/components/map.js
+++ public/js/components/map.js
@@ -4,38 +4,71 @@ const map = {
     template: `
     
     <section class="section__health" id="id__health"></section>
-    <canvas></canvas>
-
-        
+    <canvas id="canvas"></canvas>
+    
+    
     `,
+    // <img id="map__bg" src="/public/img/map.png">
     controller: ["PlayerService", function(PlayerService) {
         
         const vm = this;
         vm.id = "id__health";
         
         PlayerService.updateHealthDisplay(vm.id);
-        
+
+        vm.draw = () => {
         vm.canvas = document.querySelector('canvas');
         vm.gctx = vm.canvas.getContext("2d");
-
-        vm.gctx.fillRect(50, 50, 100, 100);
-        console.log(canvas);
-
-        // vm.gctx.beginpath();
-        // vm.gctx.setLineDash([5,5]);
-        // vm.gctx.lineDashOffset = 5;
-        // vm.gctx.moveTo(0, 100);
-        // vm.gctx.lineTo(400, 100);
-        // vm.gctx.stroke();        
+        vm.canvas.width = 800;
+        vm.canvas.height = 600;
+        vm.offset = 0;
         
-        // vm.canvas.width = window.innerWidth;
-        // vm.canvas.height = window.innerHeight;
-
+            vm.gctx.strokeStyle = "red";
+            vm.gctx.setLineDash([5,5]);
+            vm.gctx.lineWidth = 5;
+            vm.gctx.lineDashOffset = -vm.offset
+            vm.gctx.beginPath();
+            //Cerebus
+            vm.gctx.moveTo(80,470);
+            // Hades
+            vm.gctx.lineTo(160,365);
+            // Sirens
+            vm.gctx.lineTo(105,345);
+            vm.gctx.lineTo(220,260);
+            // Poseidon
+            vm.gctx.lineTo(45,260);
+            // Athena
+            vm.gctx.lineTo(115,195);
+            // Achilles
+            vm.gctx.lineTo(300,160);
+            vm.gctx.lineTo(395,330);
+            // Cyclops
+            vm.gctx.lineTo(530,540);
+            // Promethus
+            vm.gctx.lineTo(730,520);
+            vm.gctx.lineTo(740,300);
+            // Mountain
+            vm.gctx.lineTo(720,240);
+            // Zeus
+            vm.gctx.lineTo(740,55);
+            vm.gctx.stroke();
+        }   
+        function march() {
+            vm.offset++;
+            if (vm.offset > 16) {
+              vm.offset = 0;
+            }
+            vm.draw();
+            setTimeout(march, 20);
+          }
+          march();
+        
         // let background = new Image();
         // background.src = '/public/img/map.png';
-        // background.onload = function() {
-        //     vm.c.drawImage(background, 0, 0);
+        // vm.background.onload = function() {
+        //     vm.gctx.drawImage(background, 0, 0);
         // }
+        // vm.background.onload();
         // console.log(vm.canvas);
         
     }]
