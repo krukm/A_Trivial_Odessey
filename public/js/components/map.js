e4f518f6689cd554ba6c3adbd218c2580c111332
Adrean1983 <adrean_@hotmail.com>
2018-09-11 12:14:12 -0400

    added story text typewriter function

--- public/js/components/map.js
+++ public/js/components/map.js
@@ -7,12 +7,10 @@ const map = {
         <section class="img__container">
             <section class="section__health" id="id__health"></section>
             <section class="story__container">
-                <p class="story">Zeus has followed your journey from the Underworld, across the ocean and your encounters with the Cyclops and Prometheus. “I did not expect you to make it this far. You are still a mere mortal”. A lighting bolt appears in his hand. Zeus is the last thing standing between you and your loved ones!</p>
+                <p id="story" class="story"></p>
             </section>
         </section>
     </section>   
-            
-            
             `,
             
     // 
@@ -21,8 +19,21 @@ const map = {
         
         const vm = this;
         vm.id = "id__health";
-        
         PlayerService.updateHealthDisplay(vm.id);
+        
+        vm.storyText = "Zeus has followed your journey from the Underworld, across the ocean and your encounters with the Cyclops and Prometheus. “I did not expect you to make it this far. You are still a mere mortal”. A lighting bolt appears in his hand. Zeus is the last thing standing between you and your loved ones!"
+        vm.i = 0;
+        vm.speed = 40;
+
+        vm.typeWriter = () => {
+            if (vm.i < vm.storyText.length) {
+                document.getElementById("story").innerHTML += vm.storyText.charAt(vm.i);
+                vm.i++;
+                setTimeout(vm.typeWriter, vm.speed);
+                console.log("typing");
+            }
+        }
+        vm.typeWriter();
 
         vm.draw = () => {
         vm.canvas = document.querySelector('canvas');
