6b33f73b0b87cea6ab1932382bfe37e8663e030a
Adrean1983 <adrean_@hotmail.com>
2018-09-14 10:44:11 -0400

    removed additional code from map.js

--- public/js/components/map.js
+++ public/js/components/map.js
@@ -198,36 +198,3 @@ const map = {
 }
 
 angular.module('app').component('map', map);
-
-
-
-
-   vm.gctx.strokeStyle = "red";
-            vm.gctx.setLineDash([5, 5]);
-            vm.gctx.lineWidth = 5;
-            vm.gctx.lineDashOffset = -vm.offset
-            vm.gctx.beginPath();
-            //Cerebus
-            vm.gctx.moveTo(80, 470);
-            // Hades
-            vm.gctx.lineTo(160, 365);
-            // Sirens
-            vm.gctx.lineTo(105, 345);
-            vm.gctx.lineTo(220, 260);
-            // Poseidon
-            vm.gctx.lineTo(45, 260);
-            // Athena
-            vm.gctx.lineTo(115, 195);
-            // Achilles
-            vm.gctx.lineTo(300, 160);
-            vm.gctx.lineTo(395, 330);
-            // Cyclops
-            vm.gctx.lineTo(530, 540);
-            // Promethus
-            vm.gctx.lineTo(730, 520);
-            vm.gctx.lineTo(740, 300);
-            // Mountain
-            vm.gctx.lineTo(720, 240);
-            // Zeus
-            vm.gctx.lineTo(740, 55);
-            vm.gctx.stroke();
