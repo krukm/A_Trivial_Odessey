f9c4c29a1623a6a7effa425f8a745bb6d6b50b52
chelsyehannum <chelsye.hannum@gmail.com>
2018-09-14 11:06:00 -0400

    add instructions button  to intro component

--- public/js/components/intro.js
+++ public/js/components/intro.js
@@ -5,7 +5,10 @@ const intro = {
     <section class = "intro_container">
     <h1 class="h1__title">A Trivial Odyssey</h1>
     <p class="intro_text">Zeus has banished you to the Underworld and capriciously imprisoned your loved ones at Mt. Olympus. The gods it seems have made you their plaything! They will move heaven and earth so that you don’t see your family again. Use your cunning, strength and bravery to defy the gods and foil their schemes.</p>
-    <button class="button__play" ng-click="$ctrl.play()">{{ $ctrl.playButton }}</button>
+    <section class="button__intro--section">
+        <button class="button__play" ng-click="$ctrl.play()">{{ $ctrl.playButton }}</button>
+        <button class="button__instructions--intro" ng-click="$ctrl.instructions()">INSTUCTIONS</button>
+    </section>
     </section>
 
     `,
@@ -18,7 +21,11 @@ const intro = {
         }
 
         vm.play = () => {
-            $location.url("/map")
+            $location.url("/map");
+        }
+
+        vm.instructions = () => {
+            $location.url("/instructions");
         }
     }]
 }
