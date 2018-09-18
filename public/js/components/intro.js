cf649bcfdb80fcebb9c339050fd2def0920202fb
Matthew Kruk <matthew.kruk@gmail.com>
2018-09-18 13:08:30 -0400

    intro logo

--- public/js/components/intro.js
+++ public/js/components/intro.js
@@ -3,7 +3,7 @@
 const intro = {
     template: `
     <section class = "intro_container">
-    <img class="logo" src = "img/logo2.png">
+    <img class="logo" ng-src="img/logo2.png">
     <p class="intro_text">Zeus has banished you to the Underworld and capriciously imprisoned your loved ones at Mt. Olympus. The gods it seems have made you their plaything! They will move heaven and earth so that you don’t see your family again. Use your cunning, strength and bravery to defy the gods and foil their schemes.</p>
     <section class="button__intro--section">
         <button class="button__play" ng-click="$ctrl.play()">{{ $ctrl.playButton }}</button>
